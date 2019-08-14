d78cbf66b0f9ec8b3e31173b046649ccaef3b313
Maciej Krüger <mkg20001@gmail.com>
2019-08-14 16:58:53 +0200

    refactor: everything

--- README.md
+++ README.md
@@ -4,10 +4,20 @@ Simple script to download files onto your Tiptoi(r) device
 
 # Usage
 
-Run the script with a search query
+download: Run the script with a product id and a download folder. It will download the tiptoi file into that directory.
 
 ```
-$ tiptoi-download "schatzsuche in der"
+$ tiptoi-download download 55451 /tmp
 ```
 
+list: Will output a JSON list of all tiptois
 
+```
+$ tiptoi-download list
+```
+
+search: Will search for a product and output suggestions as JSON
+
+```
+$ tiptoi-download search schatzsuche
+```
--- tiptoi-download.sh
+++ tiptoi-download.sh
@@ -3,28 +3,150 @@
 set -e
 
 BASE="https://www.ravensburger.de"
+TMP=$(mktemp)
 
-query="$1"
+log() {
+  echo "$(date +%s): $*"
+}
 
-if [ -z "$query" ]; then
-  echo "No searchquery supplied!" >&2
-  exit 2
+err() {
+  echo "ERROR: $*" >&2
+}
+
+menu() {
+  i=0
+  list=()
+
+  while [ ! -z "$1" ]; do
+    list+=("$1")
+
+    echo "[$i] $1"
+    i=$((i+1))
+    shift
+  done
+
+  if [[ "$i" == "1" ]]; then
+    RES="0"
+  else
+    RES=""
+  fi
+
+  while [ -z "$RES" ]; do
+    read -p "> " sel
+    if [ ! -z "${list[$sel]}" ]; then
+      RES="$sel"
+    fi
+  done
+}
+
+jsonSelect() {
+  INPUT="$1"
+  FILTER="$2"
+
+  list=()
+  echo "$INPUT" | jq -r "$FILTER" >"$TMP"
+  while read input; do
+    list+=("$input")
+  done <"$TMP"
+
+  menu "${list[@]}"
+  item=$(echo "$INPUT" | jq ".[$RES]")
+}
+
+doSearch() {
+  query="$*"
+
+  if [ -z "$query" ]; then
+    err "No search query supplied"
+    return 2
+  fi
+
+  queryEncoded=$(echo "$query" | sed "s| |+|g") # todo: more encode
+
+  suggestions=$(curl -s "$BASE/start/searchSuggest.form?query=$queryEncoded")
+  suggestionsJSON=$(node -e 'JSON.stringify('"$suggestions"'.produkte.filter(produkt => produkt.label.startsWith("tiptoi")))' -p)
+}
+
+search() {
+  log "Loading suggestions for '$*'..."
+
+  doSearch "$@"
+  
+  if [[ "$suggestionsJSON" == "[]" ]]; then
+    err "Nothing found for tiptoi"
+    return 1
+  fi
+
+  jsonSelect "$suggestionsJSON" ".[] | .label"
+
+  log "Downloading $(echo "$item" | jq -r ".label")..."
+
+  download "$(echo "$item" | jq -r ".artikelNr")"
+}
+
+download() {
+  productId="$1"
+
+  if [ -z "$productId" ]; then
+    err "No product id supplied!"
+  fi
+
+  gme=$(curl -s "$BASE/TiptoiDownload.form?itemId=$productId" | grep -o "https.*gme" | uniq)
+  log "Downloading gme..."
+  
+  wget -O "$MNT/$(basename "$gme")" "$gme"
+}
+
+get_tiptois() {
+  tiptois=$(lsblk -Jo name,rm,size,ro,type,mountpoint,hotplug,label,uuid,model,serial,rev,vendor,hctl | jq -c '.blockdevices[] | select(.vendor == "Tiptoi  ")' | jq -sc ".")
+}
+
+device() {
+  get_tiptois
+  
+  if [[ "$tiptois" == "[]" ]]; then
+    err "No tiptois found"
+    return 2
+  fi
+
+  jsonSelect "$tiptois" ".[] | (.vendor + .model)"
+
+  MNT=$(echo "$item" | jq -r .mountpoint)
+  if [ -z "$MNT" ]; then
+    MNT=$(mktemp -d)
+    log "Mounting under $MNT..."
+    sudo mount /dev/"$(echo "$item" | jq -r .name)" "$MNT"
+  fi
+}
+
+cmd_list() {
+  get_tiptois
+  
+  echo "$tiptois"
+}
+
+cmd_search() {
+  doSearch "$@"
+
+  echo "$suggestionsJSON"
+}
+
+cmd_download() {
+  MNT="$2"
+  download "$1"
+}
+
+if [ -z "$1" ]; then
+  device
+
+  while true; do
+    read -p "Search query: " input
+    if [ ! -z "$input" ]; then
+      search "$input" || /bin/true
+    fi
+  done
+else
+  CMD="$1"
+  shift
+  "cmd_$CMD" "$@"
 fi
-
-queryEncoded=$(echo "$query" | sed "s| |+|g") # todo: more encode
-
-log "Loading suggestions for '$query'..."
-
-suggestions=$(curl "$BASE/start/searchSuggest.form?query=$queryEncoded")
-suggestionsJSON=$(node -e 'JSON.stringify('"$suggestions"')' -p)
-
-log "Filtering by tiptoi support..."
-
-
-
-href=""
-
-log "Getting gme url..."
-productId=$(echo "$href" | grep -o "[-][0-9]*/index.html" | grep -o "[0-9]*")
-gme=$(curl -s "$BASE/TiptoiDownload.form?itemId=$productId" | grep -o "https.*gme" | uniq)
-log "Downloading gme..."
